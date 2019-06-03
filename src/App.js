32d568d0b583db9e207d5aa05dcf286536a558e9
Cristian Clavijo <cristian982940@gmail.com>
2019-06-02 22:28:43 -0500

    consulting api heroku

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { async } from 'q';
 
 
 function App (){
+  const [ frase, obtenerFrase ] = useState({})
+
+  //CONSULTA A UNA REST API
+  const consultarAPI = async () =>{
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+    //agregar el resultado de la api al state similar a this.setState
+    obtenerFrase(resultado.data[0])
+
+  }
+
+  useEffect(
+    () =>{
+      consultarAPI();
+    }, []
+  )
+
+  console.log(frase)
   return(
     <h1>Hola </h1>
   )
